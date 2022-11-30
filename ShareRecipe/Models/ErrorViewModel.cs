ab6dc8575e3ad2c7101a9efcefc83ae168e7b61c
Ivan Aleksandrov <ivan.t.aleksandrov@gmail.com>
2022-11-30 23:15:38 +0200

    Add project files.

---
+++ ShareRecipe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShareRecipe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
